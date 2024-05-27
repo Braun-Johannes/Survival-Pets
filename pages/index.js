2919e8c545d09ace30a12646e0dee9961416bafc
Alexander <alexander.niehaus@gmx.net>
2024-05-27 16:35:43 +0200

    use styled Components for styling, finish ticket 1

--- pages/index.js
+++ pages/index.js
@@ -1,22 +1,36 @@
 import pets from "@/pets";
+import styled from "styled-components";
+
+const StyledList = styled.ul`
+  display: flex;
+  justify-content: space-around;
+  list-style: none;
+  padding: 0;
+  align-items: center;
+  margin: 0;
+  height: 50vh;
+`;
+
+const StyledListItem = styled.li`
+  border: 2px black solid;
+  border-radius: 5px;
+  padding: 5px;
+  cursor: pointer;
+`;
+
+const StyledHeading = styled.h1`
+  text-align: center;
+`;
 
 export default function HomePage() {
   return (
     <>
-      <div>
-        <h1>Select a Survival Pet</h1>
-      </div>
-      <div>
-        <ul className="petList">
-          {pets.map((pet) => {
-            return (
-              <li className="petListItem" key={pet.id}>
-                {pet.name}
-              </li>
-            );
-          })}
-        </ul>
-      </div>
+      <StyledHeading>Select a Survival Pet</StyledHeading>
+      <StyledList>
+        {pets.map((pet) => {
+          return <StyledListItem key={pet.id}>{pet.name}</StyledListItem>;
+        })}
+      </StyledList>
     </>
   );
 }
--- styles.js
+++ styles.js
@@ -12,27 +12,10 @@ export default createGlobalStyle`
     font-family: system-ui;
   }
 
-  h1 {
-    text-align: center;
-  }
+ 
 
-  .petList {
-    display: flex;
-    justify-content: space-around;
-    list-style: none;
-    padding: 0;
-    align-items: center;
-    margin: 0;
-    height: 50vh;
-  }
+ 
   
-  .petListItem {
-    border: 2px black solid;
-    border-radius: 5px;
-    padding: 5px;
-    cursor: pointer;
-    
-    }
   
 
 
