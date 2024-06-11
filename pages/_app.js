d282911bf0eaac4bcf74f2cb97733f1958de99cd
Johannes Braun <Braun-Johannes@outlook.de>
2024-06-11 11:42:49 +0200

    add function to handle toast message and delete

--- pages/_app.js
+++ pages/_app.js
@@ -1,10 +1,48 @@
 import GlobalStyle from "../styles";
+import { useState } from "react";
+import { nanoid } from "nanoid";
+import Toast from "@/components/Toast";
 
 export default function App({ Component, pageProps }) {
+  const [toasts, setToasts] = useState([]);
+
+  function handleAddToast(message, variant = "success") {
+    const id = nanoid();
+    setToasts((prevToasts) => [
+      ...prevToasts,
+      { id, visible: true, message, variant },
+    ]);
+
+    setTimeout(() => {
+      setToasts((prevToasts) =>
+        prevToasts.map((toast) =>
+          toast.id === id ? { ...toast, visible: false } : toast
+        )
+      );
+    }, 4500);
+
+    setTimeout(() => {
+      handleDeleteToast(id);
+    }, 5000);
+  }
+
+  function handleDeleteToast(id) {
+    setToasts((prevToasts) => prevToasts.filter((toast) => toast.id !== id));
+  }
+
   return (
     <>
       <GlobalStyle />
       <Component {...pageProps} />
+      <ul style={{ padding: 0 }}>
+        {toasts.map((toast) => (
+          <Toast
+            key={toast.id}
+            toast={toast}
+            onToastClose={handleDeleteToast}
+          />
+        ))}
+      </ul>
     </>
   );
 }
