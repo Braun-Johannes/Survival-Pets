ba96a58c9d64a608571e616810ac4c0ba22e5d32
Johannes Braun <Braun-Johannes@outlook.de>
2024-06-18 11:48:12 +0200

    added component for Images

---
+++ components/PNGImage.jsx
@@ -0,0 +1,15 @@
+import images from "@/lib/images";
+import Image from "next/image";
+
+export default function PNGImage({ variant, size = 50, ariaLabel }) {
+  const image = images[variant];
+
+  if (!image) {
+    console.error(`Image ${variant} not found`);
+    return null;
+  }
+
+  return (
+    <Image src={image.src} alt={ariaLabel} width={size} height={size}></Image>
+  );
+}
