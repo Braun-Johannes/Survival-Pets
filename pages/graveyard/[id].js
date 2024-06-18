c374658bebb5076f8442f981c24f31d992083392
Alexander <alexander.niehaus@gmx.net>
2024-06-18 12:27:14 +0200

    fix vercel bug

--- pages/graveyard/[id].js
+++ pages/graveyard/[id].js
@@ -9,6 +9,10 @@ export default function PetsDetail({ deceasedPets }) {
   const router = useRouter();
   const { id } = router.query;
 
+  if (!deceasedPets) {
+    return <div>...loading</div>;
+  }
+
   const detailsPet = deceasedPets.find((pet) => pet.id === id);
 
   if (!detailsPet) {
