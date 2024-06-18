5e8d491e9b11f20028a66fb4999603cda0cbb117
Alexander <alexander.niehaus@gmx.net>
2024-06-18 11:49:32 +0200

    layout detailspage

--- pages/graveyard/[id].js
+++ pages/graveyard/[id].js
@@ -2,6 +2,8 @@ import { useRouter } from "next/router";
 import SVGIcon from "@/components/SVGIcon";
 import { formatPetsAge } from "@/utils";
 import StyledLink from "@/components/Styles/StyledLink";
+import StyledHeading from "@/components/Styles/StyledHeading";
+import styled from "styled-components";
 
 export default function PetsDetail({ deceasedPets }) {
   const router = useRouter();
@@ -15,27 +17,67 @@ export default function PetsDetail({ deceasedPets }) {
 
   return (
     <>
-      <StyledLink href={"/graveyard"}> ←</StyledLink>
-      <h1>{detailsPet.name}</h1>
-      <br />
-      <SVGIcon
-        variant={detailsPet.type}
-        size={100}
-        ariaLabel={detailsPet.type}
-      />
-      <br />
-      <p>Name: {detailsPet.name}</p>
-      <br />
-      <p>
-        Time Alive:{" "}
-        {formatPetsAge(
-          Math.floor(detailsPet.lastUpdated - detailsPet.createdAt)
-        )}
-      </p>
-      <br />
-      <p>Creation Date: {detailsPet.birthday}</p>
-      <br />
-      <p>Death Date: {detailsPet.deathDate}</p>
+      <StyledGrid>
+        <div>
+          <StyledLink href={"/graveyard"}> ←</StyledLink>
+          <StyledHeading>{detailsPet.name}</StyledHeading>
+        </div>
+        <StyledPetContainer>
+          <SVGIcon
+            variant={detailsPet.type}
+            size={250}
+            ariaLabel={detailsPet.type}
+          />
+        </StyledPetContainer>
+        <DeceasedPetContainer>
+          <StyledP>Name: </StyledP>
+          <StyledP>{detailsPet.name}</StyledP>
+
+          <StyledP>Time Alive: </StyledP>
+          <StyledP>
+            {formatPetsAge(
+              Math.floor(detailsPet.lastUpdated - detailsPet.createdAt)
+            )}
+          </StyledP>
+
+          <StyledP>Creation Date:</StyledP>
+          <StyledP>{detailsPet.birthday}</StyledP>
+
+          <StyledP>Death Date: </StyledP>
+          <StyledP>{detailsPet.deathDate}</StyledP>
+        </DeceasedPetContainer>
+      </StyledGrid>
     </>
   );
 }
+
+const StyledPetContainer = styled.div`
+  display: flex;
+  justify-content: center;
+  align-items: center;
+`;
+
+const DeceasedPetContainer = styled.div`
+  display: grid;
+  position: fixed;
+  bottom: 0;
+  background: white;
+  grid-template-columns: auto auto;
+  border: 3px solid black;
+  border-radius: 10px;
+  width: 100vw;
+  margin-left: 1px;
+  margin-right: 1px;
+`;
+
+const StyledP = styled.p`
+  font-size: 1.5rem;
+  padding-left: 5rem;
+`;
+
+const StyledGrid = styled.div`
+  display: grid;
+  grid-template-columns: 1fr;
+  grid-template-rows: 10% auto 30%;
+  height: calc(100vh - 1rem);
+`;
