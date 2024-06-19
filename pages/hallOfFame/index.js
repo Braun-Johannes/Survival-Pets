0a590092b36dade6036706bdc2379f1ce11aa467
Jonas <wendjon@gmx.de>
2024-06-19 14:54:55 +0200

    file name

--- pages/hallOfFame/index.js
+++ pages/hallOfFame/index.js
@@ -3,6 +3,7 @@ import StyledLink from "@/components/Styles/StyledLink";
 import useSWR from "swr";
 import { formatPetsAge } from "@/utils";
 import PNGImage from "@/components/PNGImage";
+import styled from "styled-components";
 
 export default function HallOfFame() {
   const { data, isLoading } = useSWR("/api/pets");
@@ -19,15 +20,28 @@ export default function HallOfFame() {
         <StyledLink href={"/"}> ←</StyledLink>
         <StyledHeading>Hall of Fame</StyledHeading>
       </div>
-      <ul>
+      <StyledList>
         {data.map((pet) => (
-          <li key={pet._id}>
+          <StyledListItems key={pet._id}>
+            {pet.name}
             <PNGImage variant={pet.type} size={50} ariaLabel={pet.type} />
-            {pet.name}{" "}
             {formatPetsAge(Math.floor(pet.lastUpdated - pet.createdAt))}
-          </li>
+          </StyledListItems>
         ))}
-      </ul>
+      </StyledList>
     </>
   );
 }
+
+const StyledListItems = styled.li`
+  border: 2px solid black;
+  background: white;
+  margin: 10px;
+`;
+
+const StyledList = styled.ul`
+  display: flex;
+  flex-direction: column;
+  list-style: none;
+  padding: 0;
+`;
